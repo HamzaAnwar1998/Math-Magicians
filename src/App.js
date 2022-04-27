797f0b89eac0ed476f47f6c228f8b0aebde04ec6
HamzaAnwar1998 <anwarhamza919@gmail.com>
2022-04-27 13:26:57 +0500

    change app component from function to class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Calculator />
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
